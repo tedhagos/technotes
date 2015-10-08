89a3840f0d91c41036969c5b4110e862b092c6b5
Ted Hagos <tedhagos@gmail.com>
2015-10-08 23:44:51 +0800

    objc classes, methods, inheritance

--- src/inheritance.m
+++ src/inheritance.m
@@ -1,19 +1,22 @@
 #import <Foundation/Foundation.h>
 
 // class Vehicle ------------------------------
-@interface Vehicle : NSObject 
+@interface Vehicle : NSObject {
+  NSString *platenumber;
+} 
 -(NSString*) platenumber;
 -(void) setPlatenumber: (NSString*) args;
 @end
 
-@implementation Vehicle {
-  NSString *platenumber;
-}
+@implementation Vehicle 
 -(NSString*) platenumber {
   return platenumber;
 }
 -(void) setPlatenumber:(NSString*) args {
   platenumber=args;
+}
+-(void) foo { 
+  NSLog(@"private method Foo");
 }
 @end
 
@@ -30,13 +33,25 @@
 }
 @end
 
+// class What ----------------------------------
+
+@interface What : NSObject
+@end
+@implementation What
+-(void) do {
+  [[Vehicle new] foo];
+}
+@end
+
 // main ----------------------------------------
 
 int main(int argc, const char *argv[]) {
   @autoreleasepool {
-    Car *v = [Vehicle new];
+    Car *v = [Car new];
     [v setPlatenumber: @"1234"];
     NSLog(@"Car's plate number is %@", [v platenumber]);
+    [v foo]; // why is this working ?
+    [[What new] do]; // even this is working, there really is no private method
   }
   return 0;
 }
